671d435dc50aeb9af2be7dd594dc9a084630d638
Toby Hodges <tbyhdgs@gmail.com>
2020-01-08 09:11:58 +0100

    add lists chapter

---
+++ files/exercises/lists-blanks.py
@@ -0,0 +1,16 @@
+# Fill in the Blanks
+
+# Fill in the blanks so that the program below
+# produces the output shown.
+
+values = ____
+values.____(1)
+values.____(3)
+values.____(5)
+print('first time:', values)
+values = values[____]
+print('second time:', values)
+
+# Expected output:
+#   first time: [1, 3, 5]
+#   second time: [3, 5]
---
+++ files/exercises/lists-string-conversion.py
@@ -0,0 +1,9 @@
+# From Strings to Lists and Back
+
+# Given this:
+
+print('string to list:', list('tin'))
+print('list to string:', ''.join(['g', 'o', 'l', 'd']))
+
+# 1. What does list('some string') do?
+# 2. What does '-'.join(['x', 'y', 'z']) generate?
